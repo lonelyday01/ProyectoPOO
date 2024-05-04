b75cfa818c54ebe974165d86ca2de55b30b1b37a
lonelyday01 <janixiox@gmail.com>
2024-05-04 13:53:06 -0600

    add master class and child classes

---
+++ plantas/con_flor.py
@@ -0,0 +1,11 @@
+from plantas.plantas import Plantas
+
+
+class ConFlor(Plantas):
+    def __init__(self, nombre_cientifico, edad, tipo, color, nombre_local="hola"):
+        super().__init__(nombre_cientifico, edad, tipo)
+        self.color = color
+        self.nombre_local = nombre_local
+
+    def florear(self):
+        print(f"La planta {self.nombre_cientifico} del tipo {self.tipo} conocida como {self.nombre_local} esta floreando")
---
+++ plantas/plantas.py
@@ -0,0 +1,10 @@
+from seres_vivos import SeresVivos
+
+
+class Plantas(SeresVivos):
+    def __init__(self, nombre_cientifico, edad, tipo):
+        super().__init__(nombre_cientifico, edad)
+        self.tipo = tipo
+
+    def fotosintesis(self):
+        print(f"La planta {self.nombre_cientifico} del tipo {self.tipo} esta haciendo fotosintesis")
