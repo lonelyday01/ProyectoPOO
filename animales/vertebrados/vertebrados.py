699ba4800bc350c3afbe2e8d473b0a8aac74149b
Jorge Ramirez <jorgermzc16@gmail.com>
2024-05-11 19:17:32 -0600

    add vertebrados

---
+++ animales/vertebrados/vertebrados.py
@@ -0,0 +1,12 @@
+from animales.animales import Animales
+
+class Vertebrados(Animales):
+    def __init__(self, num_vertebras, ojos, patas, voca, nombre_cientifico, tipo_piel, edad):
+        super().__init__(ojos=ojos,
+                         patas=patas,
+                         voca=voca,
+                         nombre_cientifico=nombre_cientifico,
+                         tipo_piel=tipo_piel,
+                         edad=edad)
+    def me_puedo_roper(self):
+        print("Me puedo romper :c")
