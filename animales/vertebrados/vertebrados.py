7341b27480031bd72c60c92a24c64476db43daad
Jorge Ramirez <jorgermzc16@gmail.com>
2024-05-11 19:20:40 -0600

    fix flake8

--- animales/vertebrados/vertebrados.py
+++ animales/vertebrados/vertebrados.py
@@ -1,5 +1,6 @@
 from animales.animales import Animales
 
+
 class Vertebrados(Animales):
     def __init__(self, num_vertebras, ojos, patas, voca, nombre_cientifico, tipo_piel, edad):
         super().__init__(ojos=ojos,
@@ -8,5 +9,6 @@ class Vertebrados(Animales):
                          nombre_cientifico=nombre_cientifico,
                          tipo_piel=tipo_piel,
                          edad=edad)
+
     def me_puedo_roper(self):
         print("Me puedo romper :c")
